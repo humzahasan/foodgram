4a87b7626c8013ffe3940b17d4a71c1c58afe3b6
Humza Hasan <humzahasan2101@gmail.com>
2021-04-27 13:19:31 +0530

    feat : Redundant changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className='title'>
       <h1 className='title__header'>Foodgram</h1>
       <p className='title__heading'>
-        <strong> Your one stop food gallery, for all your munchy needs.</strong>
+        <strong> The one stop food gallery, for all your munchy needs.</strong>
       </p>
     </div>
   );
